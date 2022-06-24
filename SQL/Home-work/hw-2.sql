55e2073b0239803af8cee1840ae186d9dae3029d
ChristopherJohnAlappatt <ajchristopher2001@gmail.com>
2022-06-24 21:34:09 +0530

    Changes made

--- SQL/Home-work/hw-2.sql
+++ SQL/Home-work/hw-2.sql
@@ -11,7 +11,9 @@ Assignment:
 */
 CREATE TABLE Customers(
   customer_id VARCHAR NOT NULL PRIMARY KEY,
-  customer_name VARCHAR
+  customer_name VARCHAR,
+  customer_gender VARCHAR,
+  customer_loctaion VARCHAR
 );
 CREATE TABLE Orders (
   order_id VARCHAR NOT NULL PRIMARY KEY,
@@ -20,16 +22,13 @@ CREATE TABLE Orders (
   item_price float
 );
 
-INSERT INTO Customers VALUES('A-101','CHRISTOPHER');
-INSERT INTO Customers VALUES('A-102','CHRISTINA');
-INSERT INTO Customers VALUES('A-103','JOHN');
-INSERT INTO Customers VALUES('A-104','ELIZABETH');
-INSERT INTO Customers VALUES('A-105','JOSHUA');
-INSERT INTO Customers VALUES('A-106','ANNA');
+INSERT INTO Customers VALUES('A-101','CHRISTOPHER' ,'MALE' , 'MUMBAI');
+INSERT INTO Customers VALUES('A-102','CHRISTINA','FEMALE' , 'MUMBAI');
+INSERT INTO Customers VALUES('A-103','JOHN','MALE' , 'DELHI');
+INSERT INTO Customers VALUES('A-104','ELIZABETH','FEMALE' , 'KERALA');
+INSERT INTO Customers VALUES('A-105','JOSHUA','MALE' , 'KOLKATA');
+INSERT INTO Customers VALUES('A-106','ANNA','FEMALE' , 'MUMBAI');
 
-
-SELECT * FROM Customers;
-SELECT * FROM Orders;
 INSERT INTO Orders VALUES ('B-200','A-101','TABLE' ,400);
 INSERT INTO Orders VALUES ('B-201','A-103','CLOTH' ,350);
 INSERT INTO Orders VALUES ('B-202','A-101','CHAIR' ,350);
@@ -45,10 +44,30 @@ INSERT INTO Orders VALUES ('B-211','A-103','FISH-TANK' ,600);
 INSERT INTO Orders VALUES ('B-212','A-104','BAG' ,500);
 INSERT INTO Orders VALUES ('B-213','A-102','TABLE' ,400);
 
-SELECT COUNT(customer_id) AS number_of_items_ordered,order_item FROM Orders GROUP BY order_item;
-SELECT customer_id,order_item FROM Orders GROUP BY order_item;
-SELECT * FROM Orders WHERE customer_id ='A-101';
+
+SELECT * FROM Customers;
+SELECT * FROM Orders;
+
+--check how how many times a customer ordered
+SELECT customer_id,COUNT(*) AS orders_placed FROM Orders GROUP BY customer_id;
+
+--check quantity required of each item
+SELECT order_item,COUNT(customer_id) AS quantity FROM Orders GROUP BY order_item;
+
+--check the average amount a customer spends
 SELECT customer_id,AVG(item_price) FROM Orders GROUP BY customer_id;
-SELECT * FROM Orders WHERE customer_id ='A-105';
+
+--find the top spending customer from the customer list
+SELECT * FROM (SELECT customer_id,SUM(item_price) FROM Orders GROUP BY customer_id)
+LIMIT 1;
+
+--find the details of top spending customer from the customer list
+SELECT * FROM Customers WHERE customer_id IN (
+SELECT customer_id FROM (SELECT customer_id,SUM(item_price) FROM Orders GROUP BY customer_id) LIMIT 1
+);
+
+--find the details of the customers living in mumbai and purchased the table
+SELECT * FROM Customers WHERE customer_id IN (
+SELECT customer_id FROM Orders WHERE order_item = 'TABLE') AND customer_loctaion = 'MUMBAI';
 
 
